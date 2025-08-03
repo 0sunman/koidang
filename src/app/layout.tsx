7d68733545db1dc89748d8c49e2d2a29259d643a
0sunman <youyoungsun2000@gmail.com>
2025-08-03 23:32:49 +0900

    tt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
-export const metadata: Metadata = {
-  title: "KOIDANG",
-  description: "KOIDANG",
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
-  },
-};
+// export const metadata: Metadata = {
+//   title: "KOIDANG",
+//   description: "KOIDANG",
+//   viewport: {
+//     width: "device-width",
+//     initialScale: 1,
+//     maximumScale: 1,
+//     userScalable: false,
+//   },
+// };
 
 export default function RootLayout({
   children,
@@ -20,6 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
+      </Head>
       <body className={`antialiased`}>{children}</body>
     </html>
   );
