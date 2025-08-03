5cfb202d3f3f10202f342fbf2d4760631455b91b
0sunman <youyoungsun2000@gmail.com>
2025-08-03 23:22:50 +0900

    모바일 최적화

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1.0, 
+    user-scalable=0"
+      />
       <body className={`antialiased`}>{children}</body>
     </html>
   );
